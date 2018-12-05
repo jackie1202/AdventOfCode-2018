20be1b6dfb8f9613eb6775274c7912fa8dc9f2f4
jackie1202 <30788667+jackie1202@users.noreply.github.com>
2018-12-05 09:55:19 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <regex>
+#include <stdlib.h>
+#include <set>
+#include <cctype>
+#include <iomanip>
+using namespace std;
+
+int remove(string &txt, char &c)
+{
+    for(auto it = txt.begin(); it != txt.end(); )
+    {
+        if(*it == c || *it == c-32)
+        {
+            auto it2 = txt.erase(it);
+            it = it2;
+        }
+        else ++it;
+
+    }
+    return  txt.size();
+}
+
+int remove2(string &txt)
+{
+    // 32
+    bool Flg = false;
+while(!Flg)
+{
+    Flg = true;
+    for(auto it = txt.begin(); it != txt.end(); )
+    {
+        auto next = ++it;
+        --it;
+        if(abs(*next-*it)== 32)
+        {
+            auto it2 = txt.erase(it,++next);
+            it = it2;
+            Flg = false;
+        }
+        else ++it;
+    }
+    //cout << txt.size() << endl;
+}
+
+
+    return txt.size();
+
+}
+
+
+int main() {
+    ifstream readFile("../input.txt");
+    if(readFile.is_open()) cout << "ok" << endl;
+
+
+
+    map<string, vector<string>> item;
+    map<string,string> sorted;
+
+
+    string txt, tmp;
+    bool Flg = false;
+    readFile >> txt;
+
+    char c;
+
+    int min = INT_MAX, len = 0, len2;
+    for(c = 'a'; c <= 'z'; ++c)
+    {
+        tmp = txt;
+        len = remove(tmp,c);
+        len2 = remove2(tmp);
+        if(len2 < min) min = len2;
+        cout << len2 << " "<< min << endl;
+    }
+
+    cout << min << endl;
+
+
+
+
+    return 0;
+}
